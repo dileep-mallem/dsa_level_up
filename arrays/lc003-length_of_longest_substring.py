ca023708719fab9f0edded21101de101db68d31e
dileep-mallem <mailtodileepmallem@gmail.com>
2026-08-31 22:10:33 +0530

    eft Pointer * , Var SW

--- arrays/lc003-length_of_longest_substring.py
+++ arrays/lc003-length_of_longest_substring.py
@@ -1,7 +1,5 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
-
         # If char appear in Str again , then  moves left pointer to to max(j,d[s[j]+1]) -> d[s[j]] + 1 (most) , change the last Occurence index of char ,  lenght = j-i+1 
         d={}
         n=len(s)
