ef5f72b9eabc026d8d4ce30376ae2df03a6a91c1
dileep-mallem <mailtodileepmallem@gmail.com>
2026-08-31 22:37:37 +0530

    Used lambda Functjon efficiently

--- hash_table/lc347_top_k_freq_elements.py
+++ hash_table/lc347_top_k_freq_elements.py
@@ -14,14 +14,5 @@ class Solution(object):
         for i in nums : # O(n)
             d[i]=d.get(i,0)+1
         
-        # We can't sort the dictionary 
-        # so COnvert HashTable into List(key,value) and it using lamba fn
-        l=[] 
-        for key , value in d.items(): 
-            l.append((key,value))
-        s=sorted(l,key= lambda x : (x[1],x[0]),reverse=True)
-            
-        l2=[]
-        for i in s:
-            l2.append(i[0])
-        return l2[:k]
+        result=sorted(d,key=lambda x : (d[x],x),reverse = True) # (a,b)  based on a , valuef of b into result , reverse = True gets Decneding Order 
+        return result[:k]
