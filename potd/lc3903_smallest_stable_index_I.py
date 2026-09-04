7bacd5467e65b3e02e5183e3e3a6848ba539ae8d
dileep-mallem <mailtodileepmallem@gmail.com>
2026-09-04 18:43:51 +0530

    Solved both Brute ( Take max ,, min element INside Iteraition Loop , Time : O(n**2)) , Optimized : Take max and min elemnts first Iteslf then Iterate , Time : O(N)

---
+++ potd/lc3903_smallest_stable_index_I.py
@@ -0,0 +1,22 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n=len(nums)
+        # Build prefix maximums
+        pref_max = [0] * n
+        current_max = float('-inf')
+        for i in range(n):
+            current_max = max(current_max, nums[i])
+            pref_max[i] = current_max
+            
+        # Build suffix minimums
+        suff_min = [0] * n
+        current_min = float('inf')
+        for i in range(n - 1, -1, -1):
+            current_min = min(current_min, nums[i])
+            suff_min[i] = current_min
+        
+        for i in range(n) : 
+            instability_score=pref_max[i] - suff_min[i]
+            if instability_score <= k :
+                return i 
+        return -1
