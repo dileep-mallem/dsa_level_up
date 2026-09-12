fe15a391b20d28714e0db14eedf0e44051578ad5
dileep-mallem <283648134+dileep-mallem@users.noreply.github.com>
2026-09-12 11:07:39 +0530

    Easy but REvisit : YES

---
+++ Trees/lc102_level_order_traversal.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]: # type: ignore
+        
+        if not root:
+            return []
+        result=[]
+        q=deque()
+        q.append(root)
+
+        while q :
+            n=len(q)
+            currList=[]
+            for i in range(n) : 
+                currNode=q.popleft()
+                currList.append(currNode.val)
+
+                if currNode.left :
+                    q.append(currNode.left)
+                if currNode.right :
+                    q.append(currNode.right)
+                
+            result.append(currList)
+
+        return result
